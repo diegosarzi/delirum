b3f5f4e43fb9a22a71af0baa9e0c0cbf4f0e9cf3
Diego Sarzi <diego.sarzi@pixter.com.br>
2020-04-12 10:45:02 -0300

    New version follow vagalume

--- World.gd
+++ World.gd
@@ -1,3 +1,17 @@
 extends Node2D
 
+var direction
 
+func _physics_process(delta):
+	#######################################################################
+	##### ANIMATE FOLLOW VAGALUME
+	if $Vagalume.position.x < $Player.position.x:
+		$Vagalume.get_node("Sprite/AnimatedSprite").set_flip_h(false)
+		direction = -200
+	else:
+		$Vagalume.get_node("Sprite/AnimatedSprite").set_flip_h(true)
+		direction = 200
+		
+	$Tween.interpolate_property($Vagalume,"position",$Vagalume.position,$Player.position + Vector2(direction,-500),2,Tween.TRANS_LINEAR,Tween.EASE_IN_OUT)
+	$Tween.start()
+	#######################################################################
